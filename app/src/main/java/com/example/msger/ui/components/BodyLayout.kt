904a68ed5e815ab620ddbb2e5e9afe4c5121e0f0
MarcinLoj <m.lojewski1998@gmail.com>
2023-07-27 21:17:24 +0200

    replace innerPadding with modifier param

--- app/src/main/java/com/example/msger/ui/components/BodyLayout.kt
+++ app/src/main/java/com/example/msger/ui/components/BodyLayout.kt
@@ -1,21 +1,21 @@
 package com.example.msger.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.example.msger.common.extensions.toNavigationRoute
 import com.example.msger.ui.NavigationRoute
 
 @Composable
 fun BodyLayout(
-    errorMessage: String? = null,
-    innerPadding: PaddingValues,
-    snackbarHostState: SnackbarHostState? = null,
     route: String,
+    modifier: Modifier = Modifier,
+    errorMessage: String? = null,
+    snackbarHostState: SnackbarHostState? = null,
     content: @Composable (ColumnScope.() -> Unit)
 ) {
     val shouldSnackbarBeShown = when (route.toNavigationRoute()) {
@@ -23,7 +23,11 @@ fun BodyLayout(
         is NavigationRoute.Home -> false
         else -> true
     }
-    Column(modifier = Modifier.padding(innerPadding)) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
         content()
         if (shouldSnackbarBeShown) {
             Snackbar(message = errorMessage!!, snackbarHostState = snackbarHostState!!)
