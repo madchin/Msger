1336db77317bbcf8392d35927a6f797d0170176f
MarcinLoj <m.lojewski1998@gmail.com>
2023-08-06 23:01:15 +0200

    fix input validation

--- app/src/main/java/com/example/msger/feature_authentication/presentation/util/InputHelper.kt
+++ app/src/main/java/com/example/msger/feature_authentication/presentation/util/InputHelper.kt
@@ -3,8 +3,8 @@ package com.example.msger.feature_authentication.presentation.util
 import com.example.msger.R
 
 fun passwordInputSupportText(password: String): Int = when {
-    password.isBlank() -> R.string.input_blank_validation
-    !isPasswordLengthValid(password = password) -> R.string.password_input_length_validation
+    password.isNotEmpty() && password.isBlank() -> R.string.input_blank_validation
+    password.isNotEmpty() && !isPasswordLengthValid(password = password) -> R.string.password_input_length_validation
     else -> R.string.input_required
 }
 
@@ -13,7 +13,7 @@ fun confirmPasswordInputSupportText(password: String, confirmPassword: String): 
     else R.string.confirm_password_validation
 
 fun emailInputSupportText(email: String): Int = when {
-    email.isBlank() -> R.string.input_blank_validation
-    !isEmailFormatValid(email = email) -> R.string.email_input_format_validation
+    email.isNotEmpty() && email.isBlank() -> R.string.input_blank_validation
+    email.isNotEmpty() && !isEmailFormatValid(email = email) -> R.string.email_input_format_validation
     else -> R.string.input_required
 }
--- app/src/main/java/com/example/msger/feature_chat_manage/presentation/util/InputValidation.kt
+++ app/src/main/java/com/example/msger/feature_chat_manage/presentation/util/InputValidation.kt
@@ -1,5 +1,5 @@
 package com.example.msger.feature_chat_manage.presentation.util
 
-fun isChatNameValid(chatName: String): Boolean = chatName.isNotBlank()
+fun isChatNameValid(chatName: String): Boolean = chatName.isEmpty() || chatName.isNotBlank()
 
-fun isUsernameValid(username: String): Boolean = username.isNotBlank()
+fun isUsernameValid(username: String): Boolean = username.isEmpty() || username.isNotBlank()
