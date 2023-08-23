eac1dd5e861dbc6ecf4ce21e8a9d99e3593698d2
MarcinLoj <m.lojewski1998@gmail.com>
2023-08-23 18:17:16 +0200

    bump local db version

--- app/src/main/java/com/example/msger/core/data/data_source/local/AppDatabase.kt
+++ app/src/main/java/com/example/msger/core/data/data_source/local/AppDatabase.kt
@@ -9,7 +9,7 @@ import com.example.msger.feature_chat_manage.data.data_source.local.entity.ChatE
 
 @Database(
     [ChatEntity::class],
-    version = 1
+    version = 2
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun chatDao(): ChatDao
@@ -27,6 +27,7 @@ abstract class AppDatabase : RoomDatabase() {
                         AppDatabase::class.java,
                         DATABASE_NAME
                     )
+                    .fallbackToDestructiveMigration()
                     .build()
                     .also { instance = it }
             }
