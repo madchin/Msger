eca970f053012e2e17f9d8cb7fae4b4ccdfd5c8e
MarcinLoj <m.lojewski1998@gmail.com>
2023-08-29 17:25:32 +0200

    refactor: add default params for repos, use cases and create feature use cases wrappers

--- app/src/main/java/com/example/msger/feature_authentication/domain/use_case/SignUpUseCase.kt
+++ app/src/main/java/com/example/msger/feature_authentication/domain/use_case/SignUpUseCase.kt
@@ -5,7 +5,7 @@ import com.example.msger.feature_authentication.domain.repository.AuthRepository
 class SignUpUseCase(
     private val authRepository: AuthRepository
 ) {
-  suspend operator fun invoke(email: String, password: String) {
-      authRepository.signUp(email = email, password = password)
-  }
+    suspend operator fun invoke(email: String, password: String) {
+        authRepository.signUp(email = email, password = password)
+    }
 }
--- app/src/main/java/com/example/msger/feature_chat/domain/use_case/SendMessageUseCase.kt
+++ app/src/main/java/com/example/msger/feature_chat/domain/use_case/SendMessageUseCase.kt
@@ -5,5 +5,6 @@ import com.example.msger.feature_chat.domain.repository.DatabaseChatRepository
 class SendMessageUseCase(
     private val dbRepository: DatabaseChatRepository
 ) {
-    suspend operator fun invoke(chatId: String, content: String) = dbRepository.sendMessage(chatId = chatId, content = content)
+    suspend operator fun invoke(chatId: String, content: String) =
+        dbRepository.sendMessage(chatId = chatId, content = content)
 }
