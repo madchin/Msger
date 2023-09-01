46ff4eff787d884a58a1951040ddd04a6b73353c
MarcinLoj <m.lojewski1998@gmail.com>
2023-09-01 17:12:33 +0200

    feat[SignIn]: add paddings and resize inputs

--- app/src/main/java/com/example/msger/feature_authentication/presentation/sign_in/SignInScreen.kt
+++ app/src/main/java/com/example/msger/feature_authentication/presentation/sign_in/SignInScreen.kt
@@ -2,16 +2,20 @@ package com.example.msger.feature_authentication.presentation.sign_in
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.msger.core.presentation.component.ButtonLoader
 import com.example.msger.feature_authentication.presentation.util.component.InputEmail
 import com.example.msger.feature_authentication.presentation.util.component.InputPassword
 import com.example.msger.feature_authentication.presentation.util.emailInputSupportText
 import com.example.msger.feature_authentication.presentation.util.passwordInputSupportText
-import com.example.msger.core.presentation.component.ButtonLoader
 
 @Composable
 fun SignInScreen(
@@ -27,25 +31,28 @@ fun SignInScreen(
 
     Column(
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         InputEmail(
             isError = !viewModel.isEmailValid,
             value = viewModel.email,
             onValueChange = viewModel::onEmailValueChange,
             onValueClear = viewModel::onEmailValueClear,
-            supportText = emailInputSupportText
+            supportText = emailInputSupportText,
         )
         InputPassword(
             isError = !viewModel.isPasswordValid,
             value = viewModel.password,
             onValueChange = viewModel::onPasswordValueChange,
             supportText = passwordInputSupportText,
-            onDonePress = { viewModel.signInUser(openAndPopUp) }
+            onDonePress = { viewModel.signInUser(openAndPopUp) },
         )
         OutlinedButton(
             enabled = isButtonEnabled,
-            onClick = { viewModel.signInUser(openAndPopUp) }
+            onClick = { viewModel.signInUser(openAndPopUp) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
         ) {
             if (!viewModel.isLoading) {
                 Text(text = "Sign in")
@@ -53,10 +60,21 @@ fun SignInScreen(
                 ButtonLoader()
             }
         }
-        OutlinedButton(onClick = navigateToForgottenPassword) {
+        OutlinedButton(
+            onClick = navigateToForgottenPassword,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+        ) {
             Text(text = "Forgot password")
+
         }
-        Button(onClick = navigateToSignUp) {
+        Button(
+            onClick = navigateToSignUp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp, bottom = 16.dp)
+        ) {
             Text(text = "Go to sign up")
         }
     }
